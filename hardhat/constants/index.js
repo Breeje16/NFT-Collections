7efe5fd869d2de35820a80642355533efbf34507
Breeje16 <breeje2001@gmail.com>
2022-08-26 17:59:17 +0530

    Final Web Dapp

---
+++ hardhat/constants/index.js
@@ -0,0 +1,3 @@
+const WHITELIST_CONTRACT_ADDRESS = "0x5F7ca3ee624a7a8E3bD4075e5F8bC670bD6E574b";
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
